538b08bb21f61877bb1ff47365277ed89627aabb
João Paulo S. Almeida <789joaopaulo@gmail.com>
2018-02-23 15:33:13 -0300

    Generate pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,24 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SidemenuComponent } from './sidemenu/sidemenu.component';
+import { PlayerComponent } from './player/player.component';
+import { EpisodesComponent } from './episodes/episodes.component';
+import { ResultComponent } from './result/result.component';
+import { MysubscriptionsComponent } from './mysubscriptions/mysubscriptions.component';
+import { AccountComponent } from './account/account.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidemenuComponent,
+    PlayerComponent,
+    EpisodesComponent,
+    ResultComponent,
+    MysubscriptionsComponent,
+    AccountComponent
   ],
   imports: [
     BrowserModule,
